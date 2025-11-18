593391956c9316987855fa20eed3201ef31cb789
abek01sulaymonov <abek01sulaymonov@gmail.com>
2025-11-19 04:06:10 +0500

    feat: added user controller

--- controller/user.js
+++ controller/user.js
@@ -1,23 +1,63 @@
+const User = require("../models/user");
+
 const userController = {
   getUsers: (req, res) => {
-    res.send({
-      message: "Get all users",
-    });
+    User.find()
+      .select("id name email")
+      .then((users) => {
+        res.json({ users: users });
+      })
+      .catch((err) => {
+        res.json({ error: err });
+      });
   },
   getSingleUser: (req, res) => {
-    res.send({
-      message: `Get user with ID: ${req.params.id}`,
-    });
+    const userId = req.params.id;
+
+    User.findById(userId)
+      .then((user) => {
+        if (!user) {
+          return res.status(404).json({
+            message: "User not found",
+            user: null,
+          });
+        }
+        return res.status(200).json({
+          user: user,
+        });
+      })
+      .catch((err) => {
+        res.json({ error: err });
+      });
   },
   updateUser: (req, res) => {
-    res.send({
-      message: `Update user with ID: ${req.params.id}`,
-    });
+    const userId = req.params.id;
+    const updatedData = req.body;
+
+    User.findByIdAndUpdate(userId, updatedData, { new: true })
+      .then((user) => {
+        res.json({
+          message: "User updated successfully",
+          user: user,
+        });
+      })
+      .catch((err) => {
+        res.json({ error: err });
+      });
   },
   deleteUser: (req, res) => {
-    res.send({
-      message: `Delete user with ID: ${req.params.id}`,
-    });
+    const userId = req.params.id;
+
+    User.findByIdAndDelete(userId)
+      .then((user) => {
+        res.json({
+          message: "User deleted successfully",
+          user: user,
+        });
+      })
+      .catch((err) => {
+        res.json({ error: err });
+      });
   },
 };
 
